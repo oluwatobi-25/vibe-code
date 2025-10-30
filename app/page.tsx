593ff709b4e199b1d817390a56b9b719f7e589e2
Ogunsola Tobi <ogunsola.tobi@lmu.edu.ng>
2025-10-30 10:38:54 +0100

    Included a database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+
+  const user = db.user
   return (
      <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Button>Click me</Button>
