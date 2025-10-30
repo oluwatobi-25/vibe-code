9d66f214dc8df87ee645bd9ed79cbe3c0e6933c6
Ogunsola Tobi <ogunsola.tobi@lmu.edu.ng>
2025-10-30 14:25:48 +0100

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { db } from "@/lib/db";
+import UserButton from "@/features/auth/components/user-button";
 
 export default async function Home() {
 
@@ -8,6 +9,8 @@ export default async function Home() {
   return (
      <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Button>Click me</Button>
+      <UserButton/>
     </div>
+
   );
 }
